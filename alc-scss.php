2de64e7c6d9654360673ea2e0ca9495d7c12e708
Dan Fisher <dan.fisher85@yahoo.com>
2017-06-21 20:50:42 +0200

    init

---
+++ alc-scss.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Plugin Name: Alchemists SCSS Compiler
+ * Plugin URI: https://github.com/danfisher85/alc-scss
+ * Description: Compiles SCSS to CSS for Alchemists WP Theme.
+ * Version: 1.0.0
+ * Author: Dan Fisher
+ * Author URI: https://themeforest.net/user/dan_fisher
+ */
+
+
+/*
+ * 1. PLUGIN GLOBAL VARIABLES
+ */
+
+// Plugin Paths
+if (!defined('DFSCSS_THEME_DIR'))
+    define('DFSCSS_THEME_DIR', get_stylesheet_directory());
+
+if (!defined('DFSCSS_PLUGIN_NAME'))
+    define('DFSCSS_PLUGIN_NAME', trim(dirname(plugin_basename(__FILE__)), '/'));
+
+if (!defined('DFSCSS_PLUGIN_DIR'))
+    define('DFSCSS_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . DFSCSS_PLUGIN_NAME);
+
+if (!defined('DFSCSS_PLUGIN_URL'))
+    define('DFSCSS_PLUGIN_URL', WP_PLUGIN_URL . '/' . DFSCSS_PLUGIN_NAME);
+
+// Plugin Version
+if (!defined('DFSCSS_VERSION_KEY'))
+    define('DFSCSS_VERSION_KEY', 'dfscss_version');
+
+if (!defined('DFSCSS_VERSION_NUM'))
+    define('DFSCSS_VERSION_NUM', '1.0.0');
+
+
+
+/*
+ * 2. REQUIRE DEPENDENCIES
+ *
+ *    scssphp - scss compiler
+ */
+
+include_once DFSCSS_PLUGIN_DIR . '/compiler/WP_SCSS_Compiler.php'; // SCSS Compiler (vendor)
+
+
+
+/**
+ * 3. ENQUEUE STYLES
+ */
+add_action( 'wp_enqueue_scripts', 'df_enqueue_styles', 20 );
+function df_enqueue_styles() {
+
+  // Main styles
+	wp_enqueue_style( 'df-compiled', get_template_directory_uri() . '/sass/style-skin.scss', array( 'alchemists-style' ), DFSCSS_VERSION_NUM );
+
+  // Woocommerce styles
+  wp_enqueue_style( 'df-compiled-woocommerce', get_template_directory_uri() . '/sass/woocommerce/woocommerce-skin.scss', array( 'woocommerce' ), DFSCSS_VERSION_NUM );
+
+  // Sportspress styles
+  wp_enqueue_style( 'df-compiled-sportspress', get_template_directory_uri() . '/sass/sportspress-skin.scss', array( 'alchemists-sportspress' ), DFSCSS_VERSION_NUM );
+}
+
+
+
+/**
+ * 4. PASS VARIABLES INTO COMPILER
+ */
+add_filter( 'wp_scss_variables', 'df_scss_vars', 10, 2 );
+function df_scss_vars( $vars, $handle ) {
+
+  $alchemists_data = get_option('alchemists_data');
+
+	if ( ! is_array( $vars ) ) {
+		$vars = array();
+	}
+
+	// Colors
+	$vars['color-primary'] = isset( $alchemists_data['color-primary'] ) ? $alchemists_data['color-primary'] : '#ffdc11';
+	$vars['color-primary-darken'] = isset( $alchemists_data['color-primary-darken'] ) ? $alchemists_data['color-primary-darken'] : '#ffcc00';
+  $vars['color-dark'] = isset( $alchemists_data['color-dark'] ) ? $alchemists_data['color-dark'] : '#1e2024';
+  $vars['color-dark-lighten'] = isset( $alchemists_data['color-dark-lighten'] ) ? $alchemists_data['color-dark-lighten'] : '#292c31';;
+  $vars['color-gray'] = isset( $alchemists_data['color-gray'] ) ? $alchemists_data['color-gray'] : '#9a9da2';;
+  $vars['color-2'] = isset( $alchemists_data['color-2'] ) ? $alchemists_data['color-2'] : '#31404b';;
+  $vars['color-3'] = isset( $alchemists_data['color-3'] ) ? $alchemists_data['color-3'] : '#ff7e1f';;
+  $vars['color-4'] = isset( $alchemists_data['color-4'] ) ? $alchemists_data['color-4'] : '#9a66ca';;
+
+	return $vars;
+}
